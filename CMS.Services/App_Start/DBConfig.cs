86e75db3a27e4507fff40a1266d4f0428352c80d
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-07-17 00:55:01 +0300

    Add project files.

---
+++ CMS.Services/App_Start/DBConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace CMS.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Data.Entity;
+    using Data;
+    public static class DBConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Data.DbContext, Data.Migrations.Configuration>());
+            Data.DbContext.Create().Database.Initialize(true);
+        }
+    }
+}
---
+++ Data/Data/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using CMS.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static DbContext Create()
+        {
+            return new DbContext();
+        }
+    }
+}
